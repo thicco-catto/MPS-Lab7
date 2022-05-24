e83bab90636169c21b05801c9d447e1efd8e8c5f
cristobalmartinarenas <crimarare@uma.es>
2022-05-24 11:51:39 +0200

    Incorrect test

--- src/test/java/org/mps/authentication/UserRegistraticonTest.java
+++ src/test/java/org/mps/authentication/UserRegistraticonTest.java
@@ -8,6 +8,16 @@ import static org.mockito.Mockito.times;
 public class UserRegistraticonTest {
     @Test
     public void ShouldNotRegisterUserIfDateValidateFails(){
+        var date = Mockito.mock(Date.class);
+        var password = Mockito.mock(PasswordString.class);
+        var credentialStore = Mockito.mock(CredentialStore.class);
 
+        var credentialValidator = Mockito.mock(CredentialValidator.class);
+        Mockito.when(credentialValidator.validate()).thenReturn(CredentialValidator.ValidationStatus.BIRTHDAY_INVALID);
+
+        var userRegistration = new UserRegistration();
+        userRegistration.register(date, password, credentialStore, credentialValidator);
+
+        Mockito.verify(credentialStore, times(0)).register(date, password);
     }
 }

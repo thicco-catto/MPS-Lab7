77475b002cf3275b27ea0406aa02059221e1cdf2
cristobalmartinarenas <crimarare@uma.es>
2022-05-24 12:10:02 +0200

    Added tests for password and credentialStore in CredentialValidatorTest

--- src/test/java/org/mps/authentication/CredentialValidatorTest.java
+++ src/test/java/org/mps/authentication/CredentialValidatorTest.java
@@ -1,6 +1,9 @@
 package org.mps.authentication;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CredentialValidatorTest {
     @Test
@@ -15,11 +18,39 @@ public class CredentialValidatorTest {
 
     @Test
     public void ShouldReturnPasswordInvalidIfPasswordIsInvalid(){
+        var date = Mockito.mock(Date.class);
+        Mockito.when(date.validate()).thenReturn(true);
 
+        var password = Mockito.mock(PasswordString.class);
+        Mockito.when(password.validate()).thenReturn(false);
+
+        var credentialStore = Mockito.mock(CredentialStore.class);
+        Mockito.when(credentialStore.credentialExists(date, password)).thenReturn(true);
+
+        var credentialValidator = new CredentialValidator(date, password, credentialStore);
+
+        var expectedResult = CredentialValidator.ValidationStatus.PASSWORD_INVALID;
+        var obtainedResult = credentialValidator.validate();
+
+        assertEquals(expectedResult, obtainedResult);
     }
 
     @Test
     public void ShouldReturnCredentialInvalidIfCredentialDoesNotExist(){
+        var date = Mockito.mock(Date.class);
+        Mockito.when(date.validate()).thenReturn(true);
 
+        var password = Mockito.mock(PasswordString.class);
+        Mockito.when(password.validate()).thenReturn(true);
+
+        var credentialStore = Mockito.mock(CredentialStore.class);
+        Mockito.when(credentialStore.credentialExists(date, password)).thenReturn(false);
+
+        var credentialValidator = new CredentialValidator(date, password, credentialStore);
+
+        var expectedResult = CredentialValidator.ValidationStatus.CREDENTIAL_INVALID;
+        var obtainedResult = credentialValidator.validate();
+
+        assertEquals(expectedResult, obtainedResult);
     }
 }

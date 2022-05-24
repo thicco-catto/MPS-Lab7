be0595caf75358eb1f6d8af69fd6bff88b4b66eb
cristobalmartinarenas <crimarare@uma.es>
2022-05-24 12:14:31 +0200

    added comment

--- src/test/java/org/mps/authentication/CredentialValidatorTest.java
+++ src/test/java/org/mps/authentication/CredentialValidatorTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CredentialValidatorTest {
+    //Tests
     @Test
     public void ShouldReturnValidationOkIfValidationIsCorrect() {
         Date date = Mockito.mock(Date.class);
